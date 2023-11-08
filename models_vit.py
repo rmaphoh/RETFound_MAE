e1f72fed849285dcfef5599c35e5f8c39d74f9cd
defense <1900013009@pku.edu.cn>
2023-11-08 23:21:03 +0800

    add

--- models_vit.py
+++ models_vit.py
@@ -7,9 +7,9 @@ from functools import partial
 
 import torch
 import torch.nn as nn
-
+from PIL import Image, ImageOps
 import timm.models.vision_transformer
-
+import numpy as np
 
 class VisionTransformer(timm.models.vision_transformer.VisionTransformer):
     """ Vision Transformer with support for global average pooling
@@ -45,8 +45,67 @@ class VisionTransformer(timm.models.vision_transformer.VisionTransformer):
             outcome = x[:, 0]
 
         return outcome
+    def _get_attention_map(self, img_tensor):
+        """
+        img_tensor: torch.Tensor of shape (batch_size, 3, height, width)
+        Returns attention heatmaps for each image in the batch.
+        """
+        # Ensure the model is in evaluation mode
+        self.eval()
+        
+        with torch.no_grad():
+            # Forward pass through the model up to the last block
+            B = img_tensor.shape[0]
+            x = self.patch_embed(img_tensor)
+            cls_tokens = self.cls_token.expand(B, -1, -1)
+            x = torch.cat((cls_tokens, x), dim=1)
+            x = x + self.pos_embed
+            x = self.pos_drop(x)
+
+            for blk in self.blocks[:-1]:  # excluding the last block
+                x = blk(x)
+
+            # Get the attention weights from the last block
+            last_block = self.blocks[-1]
+            x, attn_weights = last_block(x, return_attention=True)
+            
+            # We are only interested in the attention from the class token
+            attn_heatmap = attn_weights[:, 0, 1:].reshape(B, 1, 14, 14)  # Assuming an image size of 224x224
+
+        # Upsample the attention heatmap to the size of the original image
+        attn_heatmap = torch.nn.functional.interpolate(attn_heatmap, scale_factor=16, mode='bilinear', align_corners=False)
+        
+        return attn_heatmap.squeeze(1)  # Removing the single-channel dimension
 
 
+def visual_heatmap(image_path, attention_heatmap, save_path):
+    """
+    Visualizes the heatmap and overlays it on the original image using only PIL.
+    image_path: Path to the original image.
+    attention_heatmap: A 2D numpy array representing the attention weights.
+    save_path: Path to save the visualized heatmap.
+    """
+    # Open the image
+    image = Image.open(image_path).convert("RGB")
+    
+    # Normalize the attention heatmap to be between 0 and 255
+    heatmap = (attention_heatmap - attention_heatmap.min()) / (attention_heatmap.max() - attention_heatmap.min())
+    heatmap = (heatmap * 255).astype(np.uint8)
+    
+    # Create a color map using a LUT (look-up table)
+    heatmap_color = ImageOps.colorize(Image.fromarray(heatmap, mode='L'), 
+                                      black="black", white="red")
+    
+    # Resize the colorized heatmap to match the size of the original image
+    heatmap_color = heatmap_color.resize(image.size, Image.LANCZOS)
+    
+    # Superimpose the heatmap onto the original image
+    superimposed_img = Image.blend(image, heatmap_color, alpha=0.5)
+    
+    # Save and show the image
+    superimposed_img.save(save_path)
+    superimposed_img.show()  # This opens the default viewer and displays the image
+    
 def vit_large_patch16(**kwargs):
     model = VisionTransformer(
         patch_size=16, embed_dim=1024, depth=24, num_heads=16, mlp_ratio=4, qkv_bias=True,
--- segmentation_visual.py
+++ segmentation_visual.py
@@ -3,45 +3,34 @@
 # Partly revised by YZ @UCL&Moorfields
 # --------------------------------------------------------
 
-from engine_segmentation import train_one_epoch, evaluate
-import models_vit
-from util.misc import NativeScalerWithGradNormCount as NativeScaler
-from util.pos_embed import interpolate_pos_embed
-from util.datasets import ridge_segmentataion_dataset
-import util.misc as misc
-import util.lr_decay as lrd
-from timm.loss import LabelSmoothingCrossEntropy, SoftTargetCrossEntropy
-from timm.models.layers import trunc_normal_
 import argparse
-import datetime
-import json
 import numpy as np
-import os
-import time
+import os,json
 from pathlib import Path
-from models_mae import SegmentationViT
+
 import torch
 import torch.backends.cudnn as cudnn
-from torch.utils.tensorboard import SummaryWriter
-from torchvision import transforms
-from util.datasets import CropPadding
+
 import timm
-from timm.data.constants import IMAGENET_DEFAULT_MEAN, IMAGENET_DEFAULT_STD
-assert timm.__version__ == "0.3.2"  # version check
-from PIL import Image
+
+assert timm.__version__ == "0.3.2" # version check
+
+import util.lr_decay as lrd
+import util.misc as misc
+from util.datasets import ridge_segmentataion_dataset
+from util.pos_embed import interpolate_pos_embed
+
+import models_vit
+from models_vit import visual_heatmap
+
+
 
 def get_args_parser():
-    parser = argparse.ArgumentParser(
-        'MAE fine-tuning for image classification', add_help=False)
+    parser = argparse.ArgumentParser('MAE fine-tuning for image classification', add_help=False)
     parser.add_argument('--data_path', default='../autodl-tmp/dataset_ROP', type=str,
                         help='dataset path')
     parser.add_argument('--split_name', default='1', type=str,
                         help='which split to load')
-    parser.add_argument('--batch_size', default=64, type=int,
-                        help='Batch size per GPU (effective batch size is batch_size * accum_iter * # gpus')
-    parser.add_argument('--epochs', default=50, type=int)
-    parser.add_argument('--accum_iter', default=1, type=int,
-                        help='Accumulate gradient iterations (for increasing the effective batch size under memory constraints)')
 
     # Model parameters
     parser.add_argument('--model', default='vit_large_patch16', type=str, metavar='MODEL',
@@ -56,44 +45,9 @@ def get_args_parser():
     # Optimizer parameters
     parser.add_argument('--clip_grad', type=float, default=None, metavar='NORM',
                         help='Clip gradient norm (default: None, no clipping)')
-    parser.add_argument('--weight_decay', type=float, default=0.05,
-                        help='weight decay (default: 0.05)')
-
-    parser.add_argument('--lr', type=float, default=None, metavar='LR',
-                        help='learning rate (absolute lr)')
-    parser.add_argument('--blr', type=float, default=1e-3, metavar='LR',
-                        help='base learning rate: absolute_lr = base_lr * total_batch_size / 256')
-    parser.add_argument('--layer_decay', type=float, default=0.75,
-                        help='layer-wise lr decay from ELECTRA/BEiT')
-
-    parser.add_argument('--min_lr', type=float, default=1e-6, metavar='LR',
-                        help='lower lr bound for cyclic schedulers that hit 0')
-
-    parser.add_argument('--warmup_epochs', type=int, default=10, metavar='N',
-                        help='epochs to warmup LR')
-
-    # Augmentation parameters
-    parser.add_argument('--color_jitter', type=float, default=None, metavar='PCT',
-                        help='Color jitter factor (enabled only when not using Auto/RandAug)')
-    parser.add_argument('--aa', type=str, default='rand-m9-mstd0.5-inc1', metavar='NAME',
-                        help='Use AutoAugment policy. "v0" or "original". " + "(default: rand-m9-mstd0.5-inc1)'),
-    parser.add_argument('--smoothing', type=float, default=0.1,
-                        help='Label smoothing (default: 0.1)')
-
-    # * Random Erase params
-    parser.add_argument('--reprob', type=float, default=0.25, metavar='PCT',
-                        help='Random erase prob (default: 0.25)')
-    parser.add_argument('--remode', type=str, default='pixel',
-                        help='Random erase mode (default: "pixel")')
-    parser.add_argument('--recount', type=int, default=1,
-                        help='Random erase count (default: 1)')
-    parser.add_argument('--resplit', action='store_true', default=False,
-                        help='Do not random erase first (clean) augmentation split')
 
     # * Finetuning params
-    parser.add_argument('--finetune', default='', type=str,
-                        help='finetune from checkpoint')
-    parser.add_argument('--task', default='', type=str,
+    parser.add_argument('--model_path', default='',type=str,
                         help='finetune from checkpoint')
     parser.add_argument('--global_pool', action='store_true')
     parser.set_defaults(global_pool=True)
@@ -101,39 +55,16 @@ def get_args_parser():
                         help='Use class token instead of global pool for classification')
 
     # Dataset parameters
-    parser.add_argument('--class_number', default=2, type=int,
+    parser.add_argument('--nb_classes', default=2, type=int,
                         help='number of the classification types')
-    parser.add_argument('--seg_number', default=1, type=int,
-                        help='number of the segmentation types')
-    parser.add_argument('--output_dir', default='./output_dir',
+
+    parser.add_argument('--save_dir', default='./output_dir',
                         help='path where to save, empty for no saving')
-    parser.add_argument('--log_dir', default='./output_dir',
-                        help='path where to tensorboard log')
+    
     parser.add_argument('--device', default='cuda',
                         help='device to use for training / testing')
     parser.add_argument('--seed', default=0, type=int)
-    parser.add_argument('--resume', default='',
-                        help='resume from checkpoint')
 
-    parser.add_argument('--start_epoch', default=0, type=int, metavar='N',
-                        help='start epoch')
-    parser.add_argument('--eval', action='store_true',
-                        help='Perform evaluation only')
-    parser.add_argument('--dist_eval', action='store_true', default=False,
-                        help='Enabling distributed evaluation (recommended during training for faster monitor')
-    parser.add_argument('--num_workers', default=10, type=int)
-    parser.add_argument('--pin_mem', action='store_true',
-                        help='Pin CPU memory in DataLoader for more efficient (sometimes) transfer to GPU.')
-    parser.add_argument('--no_pin_mem', action='store_false', dest='pin_mem')
-    parser.set_defaults(pin_mem=True)
-
-    # distributed training parameters
-    parser.add_argument('--world_size', default=1, type=int,
-                        help='number of distributed processes')
-    parser.add_argument('--local_rank', default=-1, type=int)
-    parser.add_argument('--dist_on_itp', action='store_true')
-    parser.add_argument('--dist_url', default='env://',
-                        help='url used to set up distributed training')
 
     return parser
 
@@ -152,130 +83,41 @@ def main(args):
     np.random.seed(seed)
 
     cudnn.benchmark = True
-
-    dataset_train = ridge_segmentataion_dataset(
-        data_path=args.data_path, split='train', split_name=args.split_name)
-    dataset_val = ridge_segmentataion_dataset(
-        data_path=args.data_path, split='val', split_name=args.split_name)
     dataset_test = ridge_segmentataion_dataset(
         data_path=args.data_path, split='test', split_name=args.split_name)
 
-    if True:  # args.distributed:
-        num_tasks = misc.get_world_size()
-        global_rank = misc.get_rank()
-        sampler_train = torch.utils.data.DistributedSampler(
-            dataset_train, num_replicas=num_tasks, rank=global_rank, shuffle=True
-        )
-        print("Sampler_train = %s" % str(sampler_train))
-        if args.dist_eval:
-            if len(dataset_val) % num_tasks != 0:
-                print('Warning: Enabling distributed evaluation with an eval dataset not divisible by process number. '
-                      'This will slightly alter validation results as extra duplicate entries are added to achieve '
-                      'equal num of samples per-process.')
-            sampler_val = torch.utils.data.DistributedSampler(
-                dataset_val, num_replicas=num_tasks, rank=global_rank, shuffle=True)  # shuffle=True to reduce monitor bias
-        else:
-            sampler_val = torch.utils.data.SequentialSampler(dataset_val)
-
-        if args.dist_eval:
-            if len(dataset_test) % num_tasks != 0:
-                print('Warning: Enabling distributed evaluation with an eval dataset not divisible by process number. '
-                      'This will slightly alter validation results as extra duplicate entries are added to achieve '
-                      'equal num of samples per-process.')
-            sampler_test = torch.utils.data.DistributedSampler(
-                dataset_test, num_replicas=num_tasks, rank=global_rank, shuffle=True)  # shuffle=True to reduce monitor bias
-        else:
-            sampler_test = torch.utils.data.SequentialSampler(dataset_test)
-
-    if global_rank == 0 and args.log_dir is not None and not args.eval:
-        os.makedirs(args.log_dir, exist_ok=True)
-        log_writer = SummaryWriter(log_dir=args.log_dir+args.task)
-    else:
-        log_writer = None
-
-
+    data_loader_test = torch.utils.data.DataLoader(
+        dataset_test, 
+        batch_size=args.batch_size,
+        num_workers=args.num_workers,
+        pin_memory=args.pin_mem,
+        drop_last=False
+    )
     model = models_vit.__dict__[args.model](
-        num_classes=args.class_number,
-        seg_class=args.seg_number,
+        img_size=args.input_size,
+        num_classes=args.nb_classes,
         drop_path_rate=args.drop_path,
         global_pool=args.global_pool,
     )
 
-    if args.finetune and not args.eval:
-        encoder_model_path='./finetune_rop/checkpoint-best.pth'
-        if os.path.isfile(encoder_model_path):
-            checkpoint = torch.load(encoder_model_path, map_location='cpu')
-            print(f"Load pre-trained checkpoint from: {encoder_model_path}")
-            checkpoint_model = checkpoint['model']
-            model_dict = model.state_dict()
-            loaded_dict={}
-            for k,v in checkpoint_model.items():
-                if k not in model_dict.keys():
-                    print(f"parameter: {k} not in model state dict")
-                    continue
-                loaded_dict[k]=v
-            # load pre-trained model
-            msg = model.load_state_dict(loaded_dict, strict=False)
-        else:
-            raise
+    checkpoint = torch.load(args.model_path, map_location='cpu')
+    checkpoint_model = checkpoint['model']
+    # interpolate position embedding
+    interpolate_pos_embed(model, checkpoint_model)
+    # load pre-trained model
+    msg = model.load_state_dict(checkpoint_model, strict=False) 
     model.to(device)
-    model.eval()
-    
-    data_path=args.data_path
-    img_transforms=transforms.Compose([
-        CropPadding(),
-        transforms.Resize((224,224)),
-        transforms.ToTensor(),
-        transforms.Normalize(
-                mean=IMAGENET_DEFAULT_MEAN,
-                std=IMAGENET_DEFAULT_STD)
-    ])
-    with open(os.path.join(data_path,'annotations.json'),'r') as f:
+    with open(os.path.join(args.data_path,'annotations.json'),'r') as f:
         data_dict=json.load(f)
-    with open(os.path.join(data_path,'split',f'{args.split_name}.json'),'r') as f:
-        split_list=json.load(f)['test']
-    with torch.no_grad():
-        for image_name in split_list:
-            data=data_dict[image_name]
-            
-            if data['stage']==0:
-                continue
-            img = Image.open(data['image_path']).convert('RGB')
-            img_tensor = img_transforms(img)
-            
-            img=img_tensor.unsqueeze(0).to(device)
-            cls_token,output_img = model(img)
-            output_img=output_img.squeeze().cpu()
-            # Resize the output to the original image size
-            print(output_img.shape)
-            output_img=torch.sigmoid(output_img)
-            # output_img=torch.where(output_img>=0.5,torch.ones_like(output_img),torch.zeros_like(output_img))
-            visual_mask(data['image_path'],output_img,save_path='./experiments/'+image_name)
-
-def visual_mask(image_path, mask,save_path='./tmp.jpg'):
-    # Open the image file.
-    image = Image.open(image_path).convert("RGBA")  # Convert image to RGBA
-    image= CropPadding()(image)
-    image=transforms.Resize((224,224))(image)
-    # Create a blue mask.
-    mask_np = np.array(mask)
-    mask_blue = np.zeros((mask_np.shape[0], mask_np.shape[1], 4), dtype=np.uint8)  # 4 for RGBA
-    mask_blue[..., 2] = 255  # Set blue channel to maximum
-    mask_blue[..., 3] = (mask_np * 127.5).astype(np.uint8)  # Adjust alpha channel according to the mask value
-
-    # Convert mask to an image.
-    mask_image = Image.fromarray(mask_blue)
-
-    # Overlay the mask onto the original image.
-    composite = Image.alpha_composite(image, mask_image)
-
-    # Convert back to RGB mode (no transparency).
-    rgb_image = composite.convert("RGB")
-
-    # Save the image with mask to the specified path.
-    rgb_image.save(save_path)
-
-   
+    for x,label,image_names in data_loader_test:
+        x=x.cuda()
+        att_map=model._get_attention_map(x)
+        for att,image_name in zip(att_map,image_names):
+            visual_heatmap(
+                image_path=data_dict[image_name]['image_path'],
+                attention_heatmap=att,
+                save_path=os.path.join(args.save_dir,image_name)
+            )
 if __name__ == '__main__':
     args = get_args_parser()
     args = args.parse_args()
